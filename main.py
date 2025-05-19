75abbe97374c28ad5d14ee9b92a6ca62197449ce
Roberto Lo Faro <rlofaro@users.noreply.github.com>
2025-05-20 01:15:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ except:
     locale.setlocale(locale.LC_TIME, 'it_IT')
 
 # Formattazione delle date nel formato italiano (gg/mm/aaaa)
-st.subheader(f"Gradi Giorno dal {data_inizio.strftime('%d/%m/%Y')} al {data_fine.strftime('%d/%m/%Y')}: {somma_GG:.1f}")
+st.subheader(f"Gradi Giorno dal periodo: {somma_GG:.1f}")
 
 # ---- Visualizzazione grafico ----
 st.header("Grafico Gradi Giorno")
